d52381546e8816561c650568ee0f56c9c55e9bdd
bernardoeder881 <eder.bernardo@aluno.senac.rio>
2026-04-14 21:49:13 -0300

    atividade 02

--- atvidade02.py
+++ atvidade02.py
@@ -1,21 +1,26 @@
 resposta = 'S'
 valor = 0
 desconto = 0
+valortotal = 0
 
 while resposta != 'N':
-    valor = int(input('Informe o valor da venda: '))
+    valor = int(input('Informe o valor do produto: '))
     if valor > 1000:
         desconto = valor * 0.1
-        valorfinal = valor - desconto
+        valorvenda = valor - desconto
         print(f'Valor da venda = R${valor}')
         print(f'Valor do desconto = R${desconto}')
-        print(f'Valor a pagar = R${valorfinal}')
+        print(f'Valor a pagar = R${valorvenda}')
     else:
-       print(f'Valor a pagar = R${valor}')
-     
+      desconto = 0
+      valorvenda = valor - desconto
+      print(f'Valor a pagar = R${valorvenda}')
+    valortotal += valorvenda
+    print(f' Total parcial = R${valortotal}') 
     resposta = input('Continuar S ou N?').upper().strip()
     
 print("Venda finalizada")
+print(f'Valor Total de R$ {valortotal}') 
 
 
    
