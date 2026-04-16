3d94da6250bee9e5784baae7246b9c57dc498222
bernardoeder881 <eder.bernardo@aluno.senac.rio>
2026-04-16 18:50:11 -0300

    atividade 02

--- atvidade02.py
+++ atvidade02.py
@@ -4,21 +4,22 @@ desconto = 0
 valortotal = 0
 
 while resposta != 'N':
-    valor = int(input('Informe o valor do produto: '))
-    if valor > 1000:
-        desconto = valor * 0.1
-        valorvenda = valor - desconto
-        print(f'Valor da venda = R${valor}')
-        print(f'Valor do desconto = R${desconto}')
-        print(f'Valor a pagar = R${valorvenda}')
-    else:
-      desconto = 0
+  valor = int(input('Informe o valor do produto: '))
+  if valor > 1000:
+      desconto = valor * 0.1
       valorvenda = valor - desconto
+      print(f'Valor da venda = R${valor}')
+      print(f'Valor do desconto = R${desconto}')
       print(f'Valor a pagar = R${valorvenda}')
-    valortotal += valorvenda
-    print(f' Total parcial = R${valortotal}') 
-    resposta = input('Continuar S ou N?').upper().strip()
-    
+  else:
+    desconto = 0
+    valorvenda = valor - desconto
+    print(f'Valor a pagar = R${valorvenda}')
+  valortotal += valorvenda
+  print(f' Total parcial = R${valortotal}') 
+  resposta = input('Continuar S ou N?').upper().strip()
+  while resposta != 'S' and resposta != 'N': 
+     resposta = input('Continuar S ou N?').upper().strip() 
 print("Venda finalizada")
 print(f'Valor Total de R$ {valortotal}') 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,24 @@
+resposta = 'S'
+valor = 0
+desconto = 0
+valortotal = 0
 
-    n4 = float(input('Nota 4: '))
+while resposta != 'N':
+  valor = int(input('Informe o valor do produto: '))
+  if valor > 1000:
+      desconto = valor * 0.1
+      valorvenda = valor - desconto
+      print(f'Valor da venda = R${valor}')
+      print(f'Valor do desconto = R${desconto}')
+      print(f'Valor a pagar = R${valorvenda}')
+  else:
+    desconto = 0
+    valorvenda = valor - desconto
+    print(f'Valor a pagar = R${valorvenda}')
+  valortotal += valorvenda
+  print(f' Total parcial = R${valortotal}') 
+  resposta = input('Continuar S ou N?').upper().strip()
+    while resposta != 'S':
+     resposta = input('Continuar S ou N?').upper().strip() 
+print("Venda finalizada")
+print(f'Valor Total de R$ {valortotal}') 
